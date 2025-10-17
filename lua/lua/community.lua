d83f1d24db65d20413c37d018fd269410c178e5d
oelhan <oelhan3@gmail.com>
2025-10-17 09:07:27 +0200

    in use config 17/10/2025

---
+++ lua/lua/community.lua
@@ -0,0 +1,21 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.java" }, -- add Java language support
+  { import = "astrocommunity.lsp.nvim-java" }, -- opt-in to nvim-java for LSP
+}
+
+--  { import = "astrocommunity.completion.copilot-lua-cmp" },
---
+++ lua/lua/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Treesitter
+
+---@type LazySpec
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim", 
+      "rust", 
+      "ron", 
+      "javascript", 
+      "typescript"
+      -- add more arguments for adding more treesitter parsers
+    },
+  },
+}
